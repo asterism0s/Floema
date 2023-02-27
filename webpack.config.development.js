93f588418a863aa35d404f5c8b0101510d596486
asterism0s <sarahtradsocial@gmail.com>
2023-02-27 17:38:22 +0100

    setting up the project build structure

--- webpack.config.development.js
+++ webpack.config.development.js
@@ -3,19 +3,18 @@ const path = require('path')
 
 const config = require ('./webpack.config')
 
-//setting the mode of the webpack 
-module.exports = merge  (config, {
-     //informing the webpack that it is running the build type development, not anything else. 
+module.exports = merge (config, {
      mode: 'development',
 
-     //inspect the elements in the html
-     devtool:'inline-source-map',
+     devtool: 'inline-source-map',
 
-     devsServer: {
-          writeToDisk: true
+     devServer: {
+          devMiddleware: {
+               writeToDisk: true,
+          }
      },
 
      output: {
-          path: path.resolve (__dirname, 'public')
+          path: path.resolve(__dirname, 'public')
      }
 })
--- webpack.config.js
+++ webpack.config.js
@@ -9,5 +9,3 @@ const IS_DEVELOPMENT = process.env.NODE_ENV === 'dev'
 const dirApp = path.join(__dirname, 'app')
 const dirAssets = path.join(__dirname, 'assets')
 const dirStyles = path.join(__dirname, 'styles')
-
-console.log(dirApp, dirAssets, dirStyles)
