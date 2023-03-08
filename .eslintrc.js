f7b03bc28a03266d45a1c5496939d8c507991b1a
asterism0s <sarahtradsocial@gmail.com>
2023-03-08 16:58:56 +0100

    eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+     root: true,
+     extends: ['standard'],
+     flobals: {
+          'IS_DEVELOPMENT': 'readonly'
+     },
+
+     parserOptions: {
+          ecmasVErsion: 2020
+     }
+
+}
