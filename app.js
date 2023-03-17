b7e279deda40aef03d1413d4b1aa3b6ae73a7d5b
asterism0s <sarahtradsocial@gmail.com>
2023-03-17 17:15:37 +0100

    Setting up prismic

--- app.js
+++ app.js
@@ -5,6 +5,34 @@ const app = express()
 const path = require('path')
 const port = 3000
 
+const Prismic = require('@prismicio/client')
+const PrismicDOM = require('prismic-dom')
+
+const initApi = req => {
+  return Prismic.getAPI(process.env.PRISMIC_ENDPOINT, {
+    acessToken: process.env.PRISMIC_ACESS_TOKEN,
+    req
+  })
+}
+
+const handleLinkResolver = doc => {
+  // if (doc.type === 'page') {
+  //   return '/page/' + doc.uid
+  // } else if (doc.type === 'blog_post') {
+  //   return '/blog/' + doc.uid
+  // }
+  // return '/'
+}
+
+app.use((req, res, next) => {
+  res.locals.ctx = {
+    endpoint: process.env.PRISMIC_ENDPOINT,
+    linkResolver: handleLinkResolver
+  }
+  res.locals.PrismicDOM = PrismicDOM
+  next()
+})
+
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
@@ -13,6 +41,15 @@ app.get('/', (req, res) => {
 })
 
 app.get('/about', (req, res) => {
+  initApi(req).then(api => {
+    api.query(
+      Prismic.Predicates.at('document.type', 'homepage')
+    ).then(response => {
+      res.render('pages/about', {
+        document: response.results[0]
+      })
+    })
+  })
   res.render('pages/about')
 })
 
