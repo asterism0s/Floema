9dbcf8f595706b9b3584b9bc2352624456f417bf
asterism0s <sarahtradsocial@gmail.com>
2023-06-15 10:23:01 +0200

    Starting the JS implementation

--- app/index.js
+++ app/index.js
@@ -1,1 +1,31 @@
-console.log('Hello world')
+import About from 'pages/About';
+import Collections from 'pages/Collections';
+import Detail from 'pages/Detail';
+import Home from 'pages/Home';
+class App {
+    constructor () {
+        this.createContent();
+        this.createPages();
+    }
+
+    createContent() {
+        this.content = document.querySelector('.content');
+        this.template = this.content.getAttribute('data-template'); // this.content.dataset.template - not surported by some safari versions
+
+
+    }
+
+    createPages() {
+        this.pages = {
+            about: new About(),
+            collections: new Collections(),
+            detail: new Detail(),
+            home: new Home(),
+        };
+        
+        this.page = this.pages[this.template]
+        console.log(this.page);
+    }
+};
+
+new App();
---
+++ app/pages/About/index.js
@@ -0,0 +1,9 @@
+export default class About {
+    constructor () {
+        console.log('About')
+    }
+
+    // create () {
+
+    // } //select all the things that are in the page
+}
