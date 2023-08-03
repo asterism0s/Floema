d26ffca0eb3d72429075357b4a7304fdbb2a70d5
asterism0s <sarahtradsocial@gmail.com>
2023-08-03 15:21:42 +0200

    Implementing UA Parser for phone, tablet & desktop

--- app.js
+++ app.js
@@ -1,137 +1,146 @@
-require('dotenv').config()
+require('dotenv').config();
 
-const express = require('express')
+const express = require('express');
+const logger = require('morgan');
+const errorHandler = require('errorhandler');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 
-const bodyParser = require('body-parser')
-const errorHandler = require('errorhandler')
-const logger = require('morgan')
-const methodOverride = require('method-override')
 
-const app = express()
-const path = require('path')
-const port = 3000
+const app = express();
+const path = require('path');
+const port = 3000;
 
-const Prismic = require('@prismicio/client')
-const PrismicDOM = require('prismic-dom')
+const Prismic = require('@prismicio/client');
+const PrismicDOM = require('prismic-dom');
+const UAParser = require('ua-parser-js');
 
 const initApi = req => {
   return Prismic.getApi(process.env.PRISMIC_ENDPOINT, {
     accessToken: process.env.PRISMIC_ACCESS_TOKEN,
     req
-  })
-}
+  });
+};
 
 const handleLinkResolver = doc => {
   if (doc.type === 'product') {
     return `/detail/${doc.slug}`
-  }
+  };
 
   if (doc.type === 'collections') {
     return '/collections'
-  }
+  };
 
   if (doc.type === 'about') {
     return '/about'
-  }
+  };
 
   console.log(doc)
   return '/'
-}
+};
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(errorHandler())
-app.use(logger('dev'))
-app.use(methodOverride())
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(errorHandler());
+app.use(logger('dev'));
+app.use(methodOverride());
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use((req, res, next) => {
-  res.locals.Link = handleLinkResolver
+
+  const ua = UAParser(req.headers['user-agent']);
+
+  res.locals.isDesktop = ua.device.type === undefined;
+  res.locals.isPhone = ua.device.type === 'mobile';
+  res.locals.isTablet = ua.device.type === 'tablet';
+
+  res.locals.Link = handleLinkResolver;
 
   res.locals.Numbers = index => {
     return index === 0 ? 'One' : index === 1 ? 'Two' : index === 2 ? 'Three' : index === 3 ? 'Four' : ''
-  }
+  };
 
-  res.locals.PrismicDOM = PrismicDOM
+  res.locals.PrismicDOM = PrismicDOM;
 
-  next()
-})
+  next();
+});
 
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
-// -----PAGES IMPLEMENTATION
+// PAGES IMPLEMENTATION ----------------------------------------------------------------
 const handleRequest = async api => {
-  const meta = await api.getSingle('meta')
-  const navigation = await api.getSingle('navigation')
-  const preloader = await api.getSingle('preloader')
+  const meta = await api.getSingle('meta');
+  const navigation = await api.getSingle('navigation');
+  const preloader = await api.getSingle('preloader');
 
   return {
     meta,
     navigation,
     preloader
-  }
-}
+  };
+};
 
-// -----HOME
+// HOME ----------------------------------------------------------------
 app.get('/', async (req, res) => {
-  const api = await initApi(req)
-  const defaults = await handleRequest(api)
-  const home = await api.getSingle('home')
+  const api = await initApi(req);
+  const defaults = await handleRequest(api);
+  const home = await api.getSingle('home');
 
   const { results: collections } = await api.query(Prismic.Predicates.at('document.type', 'collection'), {
     fetchLinks: 'product.image'
-  })
+  });
 
   res.render('pages/home', {
     ...defaults,
     collections,
     home
-  })
-})
+  });
+});
 
-// -----ABOUT
+// ABOUT ----------------------------------------------------------------
 app.get('/about', async (req, res) => {
-  const api = await initApi(req)
-  const defaults = await handleRequest(api)
-  const about = await api.getSingle('about')
+  const api = await initApi(req);
+  const defaults = await handleRequest(api);
+  const about = await api.getSingle('about');
 
   res.render('pages/about', {
     ...defaults,
     about
-  })
-})
+  });
+});
 
-// -----COLLECTIONS
+// COLLECTIONS ----------------------------------------------------------------
 app.get('/collections', async (req, res) => {
-  const api = await initApi(req)
-  const defaults = await handleRequest(api)
-  const home = await api.getSingle('home')
+  const api = await initApi(req);
+  const defaults = await handleRequest(api);
+  const home = await api.getSingle('home');
 
   const { results: collections } = await api.query(Prismic.Predicates.at('document.type', 'collection'), {
     fetchLinks: 'product.image'
-  })
+  });
 
   res.render('pages/collections', {
     ...defaults,
     collections,
     home
-  })
-})
+  });
+});
 
-// -----DETAILS
+// DETAILS ----------------------------------------------------------------
 app.get('/detail/:uid', async (req, res) => {
-  const api = await initApi(req)
-  const defaults = await handleRequest(api)
+  const api = await initApi(req);
+  const defaults = await handleRequest(api);
   const product = await api.getByUID('product', req.params.uid, {
     fetchLinks: 'collection.title'
-  })
+  });
 
   res.render('pages/detail', {
     ...defaults,
     product
-  })
-})
+  });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
-})
+});
