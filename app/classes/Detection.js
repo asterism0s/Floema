571c6864b83e64f6e12851ce4a8e524f3ba5a812
asterism0s <sarahtradsocial@gmail.com>
2023-08-03 17:16:43 +0200

    Implementing mobile, tablet and desktop checks

---
+++ app/classes/Detection.js
@@ -0,0 +1,35 @@
+class  Detection {
+    isPhone() {
+        if(!this.isPhoneChecked) {
+            this.isPhoneChecked = true;
+
+            this.isPhoneChecked = document.documentElement.classList.contains('phone');
+        }
+
+        return this.isPhoneChecked;
+    };
+
+    isTablet() {
+        if(!this.isTabletChecked) {
+            this.isTabletChecked = true;
+
+            this.isTabletChecked = document.documentElement.classList.contains('tablet');
+        }
+
+        return this.isTabletChecked;
+    };
+
+    isDesktop() {
+        if(!this.isDesktopChecked) {
+            this.isDesktopChecked = true;
+
+            this.isDesktopChecked = document.documentElement.classList.contains('desktop');
+        }
+
+        return this.isDesktopChecked;
+    };
+};
+
+const DetectionManager = new Detection();
+
+export default DetectionManager;
